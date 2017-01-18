4f1c430d172f78d270409942bbe33a85b059c542
Maxime Epain <maxime.epain@gmail.com>
2017-01-18 10:37:46 +0100

    Remove pods

--- Examples/Pods/Target Support Files/ClusterKit/ClusterKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ClusterKit : NSObject
-@end
-@implementation PodsDummy_ClusterKit
-@end
--- Examples/Pods/Target Support Files/Pods-ParisOpenDataUI/Pods-ParisOpenDataUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ParisOpenDataUI : NSObject
-@end
-@implementation PodsDummy_Pods_ParisOpenDataUI
-@end
